9fbe8e458ace583a0ed651df6d094b49a6fade63
Haira-Espinocilla <hdespinocilla@up.edu.ph>
2026-04-27 23:02:00 +0800

    feat: added cannon ball assets

--- src/Player1.java
+++ src/Player1.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 public class Player1 extends JPanel {
     private Image backgroundImage;
     
-    private EnemyShip enemy1 = new EnemyShip(100, 100);
-    
     private Image[] frames1 = new Image[8];
     private Image[] frames2 = new Image[8];
     
@@ -143,9 +141,7 @@ public class Player1 extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (backgroundImage != null) g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
-
-        enemy1.update(shipX, shipY); 
-        enemy1.draw(g, this);
+        
         // animation timing
         frameCounter = (frameCounter + 1) % 80;
         
@@ -162,7 +158,7 @@ public class Player1 extends JPanel {
         Image currentFrame = (frameCounter < 40) ? frames1[directionIndex] : frames2[directionIndex];
 
         if (currentFrame != null) {
-            g.drawImage(currentFrame, shipX, shipY, 200, 168, this);
+            g.drawImage(currentFrame, shipX, shipY, 240, 180, this);
         }
         
     }
